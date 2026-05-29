29d3c2b8bb1489c64cf2b0745bd66bd7c40aa3df
bibi2812005 <duongnguyendinhthang@gmail.com>
2026-05-29 21:05:43 +0700

    Add GET /api/v1/user response format

--- src/main/java/com/thangduong/shop/Main.java
+++ src/main/java/com/thangduong/shop/Main.java
@@ -10,6 +10,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int port = getPort();
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/api/v1/user", exchange -> {
+            if (!"GET".equalsIgnoreCase(exchange.getRequestMethod())) {
+                sendResponse(exchange, 405, "application/json; charset=UTF-8", "{\"code\":405,\"message\":\"Method Not Allowed\"}");
+                return;
+            }
+
+            String response = "{"
+                    + "\"code\":1000,"
+                    + "\"result\":["
+                    + "{\"id\":\"012\",\"username\":\"trongphat012\",\"fullname\":\"trongphat\",\"email\":\"trongphat@gmail.com\",\"roles\":[{\"name\":\"USER\",\"description\":\"The user is using the system\"}]},"
+                    + "{\"id\":\"123\",\"username\":\"Biz7\",\"fullname\":\"TaCanh\",\"email\":\"tacanh@gmail.com\",\"roles\":[{\"name\":\"ADMIN\",\"description\":\"The admin is managing the system\"}]}"
+                    + "]"
+                    + "}";
+            sendResponse(exchange, 200, "application/json; charset=UTF-8", response);
+        });
         server.createContext("/users", exchange -> {
             if (!"GET".equalsIgnoreCase(exchange.getRequestMethod())) {
                 sendResponse(exchange, 405, "text/plain; charset=UTF-8", "Method Not Allowed");
